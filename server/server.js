7363410f9dbb75fe04b848eb3a001a28ad31aa6d
Jayesh Bidani <jayesh.bidani@gmail.com>
2018-02-22 00:21:56 +0530

    New Changes in server/server.js

--- server/server.js
+++ server/server.js
@@ -23,6 +23,11 @@ io.on('connection', (socket) => {
 
   socket.on('createMessage', (message) => {
     console.log('Create Message',message );
+    io.emit('newMessage',{
+      from:message.from,
+      text:message.text,
+      createdAt:new Date().getTime()
+    })
   });
 
   socket.on('disconnect', () => {
